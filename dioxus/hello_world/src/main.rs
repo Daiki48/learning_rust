9645f9087ab5e048cc8e502fbb437aa63b1cef8f
Daiki48 <daiki48.engineer@gmail.com>
2022-11-04 00:30:24 +0900

    [add] Not work "exe files" after build

---
+++ dioxus/hello_world/src/main.rs
@@ -0,0 +1,11 @@
+use dioxus::prelude::*;
+
+fn app(cx: Scope) -> Element {
+    cx.render(rsx! (
+        div { "Hello, Dioxus Desktop App" }
+    ))
+}
+
+fn main() {
+    dioxus_desktop::launch(app);
+}

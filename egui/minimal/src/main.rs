01f0dcd1a339221540e1dd8f1ceec7f9ed90beaf
Daiki48 <daiki48.engineer@gmail.com>
2022-11-03 21:42:56 +0900

    [add] minimal "counter"

---
+++ egui/minimal/src/main.rs
@@ -0,0 +1,34 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use eframe::egui;
+
+#[derive(Default)]
+struct MyApp {
+    count: i32
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("This is header");
+            ui.horizontal(|ui| {
+                ui.label("This is label");
+            });
+            if ui.button("+").clicked() {
+                self.count += 1;
+            } else if ui.button("-").clicked() {
+                self.count -= 1;
+            }
+            ui.label(format!("count: {}", &self.count));
+        });
+    }
+}
+
+fn main() {
+    let options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "Minimal",
+        options,
+        Box::new(|_cc| Box::new(MyApp::default())),
+    );
+}

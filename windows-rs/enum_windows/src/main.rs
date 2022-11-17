c8bd4ed50700a240179fbb506f54523af05af35d
Daiki48 <daiki48.engineer@gmail.com>
2022-11-17 21:25:36 +0900

    [add] windows-rs sample "enum_windows"

---
+++ windows-rs/enum_windows/src/main.rs
@@ -0,0 +1,30 @@
+use windows::{
+    Win32::Foundation::{
+        BOOL,
+        HWND,
+        LPARAM,
+    },
+    Win32::UI::WindowsAndMessaging::{
+        EnumWindows,
+        GetWindowTextW,
+    }
+};
+
+extern "system" fn enum_window(window: HWND, _: LPARAM) -> BOOL {
+    unsafe {
+        let mut text: [u16; 512] = [0; 512];
+        let len = GetWindowTextW(window, &mut text);
+        let text = String::from_utf16_lossy(&text[..len as usize]);
+
+        if !text.is_empty() {
+            println!("{text}");
+        }
+        true.into()
+    }
+}
+
+fn main() -> windows::core::Result<()> {
+    unsafe {
+        EnumWindows(Some(enum_window), LPARAM(0)).ok()
+    }
+}

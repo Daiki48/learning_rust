2c4645dd3fa953bc9b0fc024bd19abf12f8c0a59
Daiki48 <daiki48.engineer@gmail.com>
2022-12-02 18:06:15 +0900

    [add] book example "6. Dialogs"

---
+++ fltk-rs/book/dialogs/src/main.rs
@@ -0,0 +1,25 @@
+#![windows_subsystem = "windows"]
+
+use fltk::{prelude::*, *};
+
+fn main() {
+    let app = app::App::default();
+    let mut wind = window::Window::default().with_size(400, 300).with_label("Dialog - example");
+
+    let mut btn = button::Button::default()
+        .with_size(80, 30)
+        .with_label("ファイルを選択")
+        .center_of_parent();
+
+    wind.end();
+    wind.show();
+
+    btn.set_callback(|_| {
+        let mut dialog = dialog::NativeFileChooser::new(dialog::FileDialogType::BrowseFile);
+        dialog.show();
+        // ファイルパスをコンソール画面に出力
+        println!("{:?}", dialog.filename());
+    });
+
+    app.run().unwrap();
+}

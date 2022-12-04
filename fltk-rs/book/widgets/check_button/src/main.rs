ba072550a8d84b34cc0ec4e694738cb0ffa3a107
Daiki48 <daiki48.engineer@gmail.com>
2022-12-04 14:03:03 +0900

    [update] btn1.set_value(true)

--- fltk-rs/book/widgets/check_button/src/main.rs
+++ fltk-rs/book/widgets/check_button/src/main.rs
@@ -7,7 +7,10 @@ fn main() {
     let app = app::App::default();
     let mut wind = window::Window::default().with_size(400, 300).with_label("Check Button");
     let flex = group::Flex::default().with_size(200, 100).column().center_of_parent();
-    let btn1 = button::CheckButton::default().with_label("check button1");
+    let mut btn1 = button::CheckButton::default().with_label("check button1");
+    // 下記のように、set_valueで初期値を設定できる。
+    // set_value(true)にすると、アプリを起動した瞬間からチェックが入っている。
+    btn1.set_value(true);
     let btn2 = button::CheckButton::default().with_label("check button2");
     let mut btn3 = button::Button::default().with_label("submit");
     flex.end();

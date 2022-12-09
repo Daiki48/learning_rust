e67bec749cf7644b67cf7bfb5f8b844a72843fcd
Daiki48 <daiki48.engineer@gmail.com>
2022-12-09 17:18:36 +0900

    :recycle: modularization

---
+++ fltk-rs/fltk-grid/example/greeting/src/greet.rs
@@ -0,0 +1,43 @@
+use fltk::{
+    prelude::*,
+    *,
+};
+use fltk_grid::Grid;
+
+pub struct Greet {
+    pub grid: Grid,
+    pub name: input::Input,
+    // btn: button::Button,
+}
+
+impl Greet {
+    pub fn default() -> Self {
+        let mut grid = Grid::default_fill();
+        grid.set_layout(8, 7);
+
+        let name = input::Input::default();
+
+        let mut g = Self {
+            grid,
+            name,
+        };
+        g.fill();
+        g
+    }
+    fn fill(&mut self) {
+        let grid = &mut self.grid;
+        grid.debug(true);
+
+        let mut title = frame::Frame::default().with_label("Greeting!");
+        title.set_frame(enums::FrameType::FlatBox);
+        title.set_color(enums::Color::DarkBlue);
+        title.set_label_color(enums::Color::White);
+        title.set_label_size(40);
+        grid.insert(&mut title, 0..5, 1);
+
+        grid.insert(&mut frame::Frame::default().with_label("name"), 2..4, 1);
+
+    }
+}
+
+
--- fltk-rs/fltk-grid/example/greeting/src/main.rs
+++ fltk-rs/fltk-grid/example/greeting/src/main.rs
@@ -2,23 +2,14 @@ use fltk::{
     prelude::*,
     *,
 };
-use fltk_grid::Grid;
+
+mod greet;
+use greet::Greet;
 
 fn main() {
     let app = app::App::default().with_scheme(app::Scheme::Gtk);
     let mut wind = window::Window::default().with_size(600, 400).with_label("Greeting App");
-    let mut grid = Grid::default_fill();
-    grid.debug(true);
-    grid.set_layout(8, 6);
-
-    let mut title = frame::Frame::default().with_label("Greeting!");
-    title.set_frame(enums::FrameType::FlatBox);
-    title.set_color(enums::Color::DarkBlue);
-    title.set_label_color(enums::Color::White);
-    title.set_label_size(40);
-    grid.insert(&mut title, 0..4, 1);
-
-    grid.insert(&mut button::Button::default(), 2..6, 1..4);
+    let mut greet = Greet::default();
 
     wind.end();
     wind.show();
